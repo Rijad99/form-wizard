0aba4e277bc4927db5acc64c0e0dd0f4353399a6
rsmajlov <rijad.smajlovic@dccs.at>
2023-02-26 14:37:15 +0100

    Add steps dinamically, add styling etc

--- src/client/components/main/FormWizard/FormWizard.tsx
+++ src/client/components/main/FormWizard/FormWizard.tsx
@@ -1,13 +1,39 @@
+import React, { useState } from 'react'
+
 import { Container } from '../../common/Container/Container';
 import { Card } from '../../common/Card/Card';
+import { StepsList } from '../StepsList/StepsList';
+import { FirstStep, SecondStep, ThirdStep, FourthStep  } from '../Steps/Steps';
 
 import styles from "../FormWizard/_FormWizard.module.scss";
 import gridStyle from "../FormGrid/_FormGrid.module.scss";
 
 export const FormWizard = () => {
+    const [step, setStep] = useState<number>(1);
+
+    const handleStepChange = (stepNumber: number) => {
+        setStep(stepNumber);
+    }
+
+    const currentStep = (step: number) => {
+        switch(step) {
+            case 1:
+                return <FirstStep />
+            case 2:
+                return <SecondStep />
+            case 3:
+                return <ThirdStep />
+            case 4:
+                return <FourthStep />
+        };
+    };
+
     return (
         <Container additionalClasses={styles.formWizardContainer}>
-            <Card additionalClasses={gridStyle.formGrid} />
+            <Card additionalClasses={gridStyle.formGrid}>
+                <StepsList onClick={() => handleStepChange(2)} />
+                {currentStep(step)}
+            </Card>
         </Container>
     )
 };
---
+++ src/client/components/main/Steps/Steps.tsx
@@ -0,0 +1,31 @@
+export const FirstStep = () => {
+    return (
+        <div>
+            <h1>First step</h1>
+        </div>
+    )
+};
+
+export const SecondStep = () => {
+    return (
+        <div>
+            <h1>Second step</h1>
+        </div>
+    )
+};
+
+export const ThirdStep = () => {
+    return (
+        <div>
+            <h1>Third step</h1>
+        </div>
+    )
+};
+
+export const FourthStep = () => {
+    return (
+        <div>
+            <h1>Fourth step</h1>
+        </div>
+    )
+};
