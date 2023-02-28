3b7a7e7783680c2d7a91afbab24e7bce23d1a55b
rsmajlov <rijad.smajlovic@dccs.at>
2023-02-28 22:00:24 +0100

    Add plan card component, css adaptions

--- src/client/components/main/Steps/SecondStepContent/SecondStepContent.tsx
+++ src/client/components/main/Steps/SecondStepContent/SecondStepContent.tsx
@@ -1,9 +1,23 @@
 import styles from '../SecondStepContent/_SecondStepContent.module.scss';
 
-const PlanCard = () => {
+import iconArcade from "../../../../../images/icons/icon-arcade.svg";
+import iconAdvanced from "../../../../../images/icons/icon-advanced.svg";
+import iconPro from "../../../../../images/icons/icon-pro.svg";
+
+interface PlanCardProps {
+    iconPath: string,
+    name: string
+}
+
+const PlanCard = (props: PlanCardProps) => {
     return (
         <div className={styles.planCard}>
-
+            <div>
+                <img src={props.iconPath} />
+            </div>
+            <div className={styles.planCardPricing}>
+                <span className={styles.planName}>{props.name}</span>
+            </div>
         </div>
     )
 }
@@ -11,7 +25,11 @@ const PlanCard = () => {
 export const SecondStepContent = () => {
     return (
         <div className={styles.stepContent1}>
-            <PlanCard />
+            <div className={styles.planCardsGrid}>
+                <PlanCard iconPath={iconArcade} name='Arcade' />
+                <PlanCard iconPath={iconAdvanced} name='Advanced' />
+                <PlanCard iconPath={iconPro} name='Pro' />
+            </div>
         </div>
     )
 };
