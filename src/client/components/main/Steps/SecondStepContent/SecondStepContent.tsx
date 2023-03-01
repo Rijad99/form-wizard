a0b7b1ffc0dd53b1d482014c9de9fb7e470fc633
Rijad99 <rijad99.sm@gmail.com>
2023-03-01 11:13:28 +0100

    Add plan card hover and click functional states

--- src/client/components/main/Steps/SecondStepContent/SecondStepContent.tsx
+++ src/client/components/main/Steps/SecondStepContent/SecondStepContent.tsx
@@ -6,29 +6,36 @@ import iconPro from "../../../../../images/icons/icon-pro.svg";
 
 interface PlanCardProps {
     iconPath: string,
-    name: string
+    name: string,
+    price: string,
+    onClick: (e: React.MouseEvent<HTMLDivElement>) => void
 }
 
 const PlanCard = (props: PlanCardProps) => {
     return (
-        <div className={styles.planCard}>
+        <div className={styles.planCard} onClick={props.onClick}>
             <div>
                 <img src={props.iconPath} />
             </div>
             <div className={styles.planCardPricing}>
                 <span className={styles.planName}>{props.name}</span>
+                <span className={styles.planPrice}>{props.price}</span>
             </div>
         </div>
     )
 }
 
 export const SecondStepContent = () => {
+    const handlePlanCardChange = (e: React.MouseEvent<HTMLDivElement>) => {
+        e.currentTarget.classList.toggle(styles.shown);
+    }
+
     return (
         <div className={styles.stepContent1}>
             <div className={styles.planCardsGrid}>
-                <PlanCard iconPath={iconArcade} name='Arcade' />
-                <PlanCard iconPath={iconAdvanced} name='Advanced' />
-                <PlanCard iconPath={iconPro} name='Pro' />
+                <PlanCard iconPath={iconArcade} name='Arcade' price='$9/mo' onClick={(e) => handlePlanCardChange(e)} />
+                <PlanCard iconPath={iconAdvanced} name='Advanced' price='$12/mo' onClick={(e) => handlePlanCardChange(e)} />
+                <PlanCard iconPath={iconPro} name='Pro' price='$15/mo' onClick={(e) => handlePlanCardChange(e)} />
             </div>
         </div>
     )
